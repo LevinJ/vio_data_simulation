5a448f3053ef8517cefc2e83f06d95ac9d4f166a
Levin Jian <jianzhirong@gmail.com>
2020-01-10 10:33:34 +0800

    midpoint integration

--- src/imu.cpp
+++ src/imu.cpp
@@ -138,21 +138,52 @@ void IMU::testImu(std::string src, std::string dist)
         MotionData imupose = imudata[i];
 
         //delta_q = [1 , 1/2 * thetax , 1/2 * theta_y, 1/2 * theta_z]
-        Eigen::Quaterniond dq;
+  /*      Eigen::Quaterniond dq;
         Eigen::Vector3d dtheta_half =  imupose.imu_gyro * dt /2.0;
         dq.w() = 1;
         dq.x() = dtheta_half.x();
         dq.y() = dtheta_half.y();
         dq.z() = dtheta_half.z();
         dq.normalize();
-        
+
         /// imu 动力学模型 欧拉积分
         Eigen::Vector3d acc_w = Qwb * (imupose.imu_acc) + gw;  // aw = Rwb * ( acc_body - acc_bias ) + gw
         Qwb = Qwb * dq;
         Pwb = Pwb + Vw * dt + 0.5 * dt * dt * acc_w;
         Vw = Vw + acc_w * dt;
+        */
         
+
         /// 中值积分
+        Eigen::Quaterniond dq;
+        Eigen::Vector3d omega;
+        if(i != imudata.size()-1){
+        	omega = 1.0/2 * (imupose.imu_gyro + imudata[i+1].imu_gyro);
+        }else{
+        	omega = imupose.imu_gyro;
+        }
+
+		Eigen::Vector3d dtheta_half =  omega * dt /2.0;
+		dq.w() = 1;
+		dq.x() = dtheta_half.x();
+		dq.y() = dtheta_half.y();
+		dq.z() = dtheta_half.z();
+		dq.normalize();
+
+		Eigen::Vector3d acc_w;
+		if(i != imudata.size()-1){
+			Eigen::Quaterniond Qwb_next = Qwb * dq;
+			acc_w = 1.0/2 *(Qwb * (imupose.imu_acc) + Qwb_next * (imudata[i+1].imu_acc)) + gw;
+		}else{
+			acc_w = Qwb * (imupose.imu_acc) + gw;  // aw = Rwb * ( acc_body - acc_bias ) + gw
+		}
+		/// imu 动力学模型, consant turn rate, constant acceleration, CTRA
+		Qwb = Qwb * dq;
+		Pwb = Pwb + Vw * dt + 0.5 * dt * dt * acc_w;
+		Vw = Vw + acc_w * dt;
+
+
+
 
         //　按着imu postion, imu quaternion , cam postion, cam quaternion 的格式存储，由于没有cam，所以imu存了两次
         save_points<<imupose.timestamp<<" "
